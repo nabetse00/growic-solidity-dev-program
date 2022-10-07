464a494ac7ce31a1308c6d8fbc2660370dfd7d72
nabetse00 <esteban.diezlaiz@gmail.com>
2022-10-07 22:54:01 +0200

    updates

--- task6-Inheritance/task6Contract.sol
+++ task6-Inheritance/task6Contract.sol
@@ -5,19 +5,21 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /// Contract
 contract Task6Contract is ERC20 {
-
     constructor() payable ERC20("Nabetse TOKEN", "NAB") {
         // fixed supply using internal _mint function
-        // here 9900 NABs initial supply
-        _mint(msg.sender, 9900); 
+        // here 9900 NABs initial supply 
+        _mint(msg.sender, 9900);
+    }
+
+    // no decimals
+    function decimals() public view virtual override returns (uint8) {
+        return 0;
     }
 
     // to support receiving ETH by default
     receive() external payable {}
 
     fallback() external payable {}
-
 }
 
 // Contract 0xB9d472fEB6FF180434F3D8b74aA3d79817DdC1C9
-// https://goerli.etherscan.io/tx/0xed78f17ccd66e02e9d2ebf7a0e8741bedfeccdd9e2926f5ae1a41799e8d6bc2a
